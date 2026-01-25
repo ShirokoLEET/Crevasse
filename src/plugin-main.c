04a48201cd1c1be098f50bff00e459490ece38b5
ShirokoLEET <2498469253@qq.com>
2026-01-26 02:43:42 +0800

    Initd 解いた手を見えなくなるまで振り続け 松开的手 会挥着 直到你再看不见

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,34 +1,71 @@
-/*
-Crevasse
-Copyright (C) 2026 @ShirokoLEET
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
-
 #include <obs-module.h>
-#include <plugin-support.h>
 
-OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+struct crevasse_filter {
+	obs_source_t *source;
+	gs_effect_t *effect;
+};
 
-bool obs_module_load(void)
+static const char *filter_getname(void *unused)
 {
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
-	return true;
+	UNUSED_PARAMETER(unused);
+	return "Crevasse - ai";
 }
 
-void obs_module_unload(void)
+static void filter_destroy(void *data)
 {
-	obs_log(LOG_INFO, "plugin unloaded");
+	struct crevasse_filter *tf = data;
+
+	if (tf) {
+		obs_enter_graphics();
+
+		gs_effect_destroy(tf->effect);
+		bfree(tf);
+
+		obs_leave_graphics();
+	}
 }
+
+static void *filter_create(obs_data_t *settings, obs_source_t *source)
+{
+	struct crevasse_filter *tf = bzalloc(sizeof(struct crevasse_filter));
+	char *effect_file;
+
+	obs_enter_graphics();
+
+	effect_file = obs_module_file("test.effect");
+
+	tf->source = source;
+	tf->effect = gs_effect_create_from_file(effect_file, NULL);
+	bfree(effect_file);
+	if (!tf->effect) {
+		filter_destroy(tf);
+		tf = NULL;
+	}
+
+	obs_leave_graphics();
+
+	UNUSED_PARAMETER(settings);
+	return tf;
+}
+
+static void filter_render(void *data, gs_effect_t *effect)
+{
+	struct crevasse_filter *tf = data;
+
+	if (!obs_source_process_filter_begin(tf->source, GS_RGBA, OBS_ALLOW_DIRECT_RENDERING))
+		return;
+
+	obs_source_process_filter_end(tf->source, tf->effect, 0, 0);
+
+	UNUSED_PARAMETER(effect);
+}
+
+struct obs_source_info crevasse_filter = {
+	.id = "crevasse_filter",
+	.type = OBS_SOURCE_TYPE_FILTER,
+	.output_flags = OBS_SOURCE_VIDEO,
+	.get_name = filter_getname,
+	.create = filter_create,
+	.destroy = filter_destroy,
+	.video_render = filter_render,
+};
